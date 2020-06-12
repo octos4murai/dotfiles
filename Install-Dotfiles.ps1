d04f2a091b3a764c4256accb398a315baf0ba9e5
Al Idian <al.idian@me.com>
2020-06-12 13:47:18 -0700

    Extend Install-Dotfiles script to work with Windows.

--- Install-Dotfiles.ps1
+++ Install-Dotfiles.ps1
@@ -1,10 +1,37 @@
 #!/usr/bin/env pwsh
 
-# PowerShell
-$sourcePath = "$HOME/.config"
-if (Test-Path $sourcePath) {
-    Remove-Item -Recurse -Force -Confirm -Path $sourcePath
+param (
+    [Parameter(Mandatory = $true)]
+    [ValidateScript({Test-Path $_ -PathType Leaf})]
+    [string]
+    $ConfigPath,
+
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("macos", "windows")]
+    [string]
+    $ProfileName
+)
+
+Get-Content $ConfigPath |
+ConvertFrom-Json |
+Select-Object -ExpandProperty "items" |
+ForEach-Object {
+    if ($_.profile -eq $ProfileName) {
+        $itemProps = @{
+            Path   = $ExecutionContext.InvokeCommand.ExpandString($_.path)
+            Target = $ExecutionContext.InvokeCommand.ExpandString($_.target)
+        }
+
+        if (!(Test-Path -Path $($itemProps.Path) -IsValid)) {
+            "The given path is not valid: '$($itemProps.Path)'."
+            continue
+        }
+
+        if (!(Test-Path $($itemProps.Target))) {
+            "The given target cannot be found: '$($itemProps.Target)'."
+            continue
+        }
+
+        New-Item @itemProps -ItemType SymbolicLink -Force
+    }
 }
-
-New-Item -ItemType SymbolicLink -Path $sourcePath -Target "$HOME/Public/Workspace/dotfiles/dotconfig"
-

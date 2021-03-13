8d478314e7e42d467bceb4f30df827da326248b7
Al Idian <al.idian@me.com>
2021-03-13 08:19:58 -0800

    Add script to update dynamic dns service

---
+++ ubuntu/update-duck-dns.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Ask Duck DNS to update their record of this machine's remote IP.
+# Note: The IP is not passed in. Instead they are expected to detect it automatically.
+
+echo url='https://www.duckdns.org/update?domains=m3g5hw3h&token=06bc1302-92e8-4e78-8aa5-abef1c34b0f6' | \
+    curl \
+        --insecure \
+        --output ~/.duckdns/duck.log \
+        --config -
